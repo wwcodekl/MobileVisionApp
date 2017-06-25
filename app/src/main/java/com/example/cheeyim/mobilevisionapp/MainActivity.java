97b72337e476d4350459ca3e84ca090d4df0dbd8
cheeyim <cheeyim@gmail.com>
2017-06-25 22:25:51 +0800

    add source

---
+++ app/src/main/java/com/example/cheeyim/mobilevisionapp/MainActivity.java
@@ -0,0 +1,237 @@
+package com.example.cheeyim.mobilevisionapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView detectedTextView;
+    Button mSend;
+    Button mLaunchBrowser;
+    Button mTakePic;
+    OkHttpClient mClient = new OkHttpClient();
+    Context mContext;
+    Uri imageUri;
+    private static final int PHOTO_REQUEST = 10;
+    private static final int REQUEST_WRITE_PERMISSION = 20;
+    private static final String SAVED_INSTANCE_URI = "uri";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        detectedTextView = (TextView) findViewById(R.id.detectedUrlTxt);
+        mSend = (Button) findViewById(R.id.btnSendMessage);
+        mContext = getApplicationContext();
+
+        mLaunchBrowser = (Button) findViewById(R.id.btnLaunchBrowser);
+        mLaunchBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowserIntent(detectedTextView.getText().toString());
+            }
+        });
+
+        mTakePic = (Button) findViewById(R.id.btnTakePic);
+        mTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this, new
+                        String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+            }
+        });
+
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    post(mContext.getString(R.string.backend_url), new  Callback(){
+
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(),"SMS Sent!",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    Call post(String url, Callback callback) throws IOException{
+        RequestBody formBody = new FormBody.Builder()
+                .add("To", mContext.getString(R.string.to_mobile))
+                .add("Body", String.format("Hey! Check out this site - %s", detectedTextView.getText().toString()))
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        Call response = mClient.newCall(request);
+        response.enqueue(callback);
+        return response;
+
+    }
+
+    private void launchBrowserIntent(String uriStr) {
+
+        Uri uriUrl = Uri.parse(uriStr);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PHOTO_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (imageUri != null) {
+            outState.putString(SAVED_INSTANCE_URI, imageUri.toString());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
+            launchMediaScanIntent();
+
+            try {
+                Bitmap textBitmap = decodeBitmapUri(this, imageUri);
+
+                detectedTextView.setText(detectedTextView.getText() + readBitmap(textBitmap));
+            }
+            catch (Exception e) {
+                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e("TEXT-API", e.toString());
+            }
+        }
+    }
+
+    private void launchMediaScanIntent() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    public void detectText(View view) {
+        Bitmap textBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pycon);
+
+        detectedTextView.setText(detectedTextView.getText() + readBitmap(textBitmap));
+    }
+
+    private String readBitmap(Bitmap textBitmap) {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+
+        if (!textRecognizer.isOperational()) {
+            new AlertDialog.Builder(this)
+                    .setMessage("Text recognizer could not be set up on your device :(")
+                    .show();
+            return null;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(textBitmap).build();
+        SparseArray<TextBlock> text = textRecognizer.detect(frame);
+        String lines = "";
+
+        for (int index = 0; index < text.size(); index++) {
+            //extract scanned text blocks here
+            TextBlock tBlock = text.valueAt(index);
+            for (Text line : tBlock.getComponents()) {
+                //extract scanned text lines here
+                String lineStr = line.getValue().toLowerCase();
+                if (lineStr.contains("http://"))
+                    lines = lineStr;
+            }
+        }
+
+        return lines;
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
+}
+
